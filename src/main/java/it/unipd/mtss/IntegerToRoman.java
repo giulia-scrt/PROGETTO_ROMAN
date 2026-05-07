d78774fe9cb61a96ee8bbc1e712d5d92af4d9816
giulia-scrt <scagiu2004@gmail.com>
2026-05-08 00:24:23 +0200

    completata struttura Maven

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,10 @@
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number) {
+        if (number <= 0 || number > 1000) {
+            throw new IllegalArgumentException("Number must be between 1 and 1000");
+        }
+        return null;
+    }
+}
---
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,22 @@
+﻿package it.unipd.mtss;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IntegerToRomanTest {
+    
+    @Test
+    public void testConvert_1_returnsI() {
+        assertEquals("I", IntegerToRoman.convert(1));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvert_0_throwsException() {
+        IntegerToRoman.convert(0);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvert_1001_throwsException() {
+        IntegerToRoman.convert(1001);
+    }
+}
