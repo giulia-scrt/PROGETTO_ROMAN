d8d4f8aa1613542709fd467b7b3d5f774ea4d575
giuls <scagiu2004@gmail.com>
2026-05-11 17:28:13 +0200

    concluso conversione IntToRoman 1-10 - close #3

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,19 +6,22 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        if (number <= 0 || number > 6) {
-            throw new IllegalArgumentException("Number must be between 1 and 6");
+        if (number <= 0 || number > 10) {
+            throw new IllegalArgumentException("Number must be between 1 and 10");
         }
-        if(number>=1 && number<=3){
-            String ris="";
-            for (int i=0; i<number; i++){
-                ris=ris+"I";
-            }
-            return ris;
+		
+    if(number>=1 && number<=3){
+        String ris="";
+        for (int i=0; i<number; i++){
+            ris=ris+"I";
         }
-		if (number==4) return "IV";
-		if (number==5) return "V";
-		if (number==6) return "VI";
-        return "";
+        return ris;
     }
+    if (number==4) return "IV";
+    if (number==5) return "V";
+    if (number>=6 && number <= 8) return "V"+IntegerToRoman.convert(number-5);
+    if (number==9) return "IX";
+    if (number==10) return "X";
+    return "";
+  }
 }
